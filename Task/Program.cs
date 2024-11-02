4e0e5ce5960c18ca4384f463e8cc30e240c1e099
Aishah999 <aishaah@code.edu.az>
2024-11-02 11:25:55 +0400

    add commit

---
+++ Task/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    static string CustomSubstring(string input,int startIndex,int length)
+    {
+        if(input==null)
+        {
+            throw new
+          ArgumentNullException(nameof(input));
+        }
+        if(startIndex<0||startIndex>=input.Length)
+        {
+            throw new
+                ArgumentOutOfRangeException(nameof(startIndex));
+        }
+        if(length<0||startIndex+length>input.Length)
+        {
+            throw new
+                ArgumentOutOfRangeException(nameof(length));
+        }
+        char[] result = new char[length];
+        for(int i=0;i<length;i++)
+        {
+            result[i] = input[startIndex + i];
+        }
+        return new string(result);
+    }
+}
+
